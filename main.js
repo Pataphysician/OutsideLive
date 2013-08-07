a41556f0af811a4c111995c9889ab42af0a8e506
Chris Gervang <gervangster@live.com>
2013-08-07 02:38:47 -0700

    pestages view updates every time a new song comes in

--- main.js
+++ main.js
@@ -198,41 +198,6 @@ if (Meteor.isServer) {
     }
   });
 
-  
-  // Meteor.publish("number-of-stages", function () {
-  //     var self = this;
-  //     var count = 0;
-  //     var initializing = true;
-
-  //     var handle = Stages.find({}).observeChanges({
-  //       added: function (id) {
-  //         count++;
-  //         if (!initializing)
-  //           self.changed("counts", {count: count});
-  //       },
-  //       removed: function (id) {
-  //         count--;
-  //         self.changed("counts", {count: count});
-  //       }
-  //       // don't care about moved or changed
-  //     });
-
-  //     // Observe only returns after the initial added callbacks have
-  //     // run.  Now return an initial value and mark the subscription
-  //     // as ready.
-  //     initializing = false;
-  //     self.added("counts", {count: count});
-  //     console.log('sub ready');
-  //     self.ready();
-
-  //     // Stop observing the cursor when client unsubs.
-  //     // Stopping a subscription automatically takes
-  //     // care of sending the client any removed messages.
-  //     self.onStop(function () {
-  //       handle.stop();
-  //     });
-  //   });
-
   /*
   Meteor.publishCounter = function(params) {
     var collection, count, handle, id, init, pub,
